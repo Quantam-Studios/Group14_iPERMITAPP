a54871d08f1bbef467eb8b5b20488f22487ecde2
Zach Goodson <quantamstudios@gmail.com>
2026-04-07 19:42:45 -0500

    Add Fields to RequestStatus

--- Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/RequestStatus.java
+++ Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/RequestStatus.java
@@ -1,5 +1,22 @@
 package edu.mizzou.Group14_iPERMITAPP.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Data
 public class RequestStatus {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String permitRequestStatus;
+    private Date date;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "permit_request_id")
+    private PermitRequest permitRequest;
 }
