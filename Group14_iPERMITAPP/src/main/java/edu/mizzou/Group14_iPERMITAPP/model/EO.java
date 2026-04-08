1931f2748eecc57032a5a7b72aab68a84c39d4a5
Zach Goodson <quantamstudios@gmail.com>
2026-04-07 19:38:40 -0500

    Update EO Fields

--- Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/EO.java
+++ Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/model/EO.java
@@ -1,5 +1,15 @@
 package edu.mizzou.Group14_iPERMITAPP.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class EO {
 
+    @Id
+    private String id;
+
+    private String name;
+    private String password;
 }
