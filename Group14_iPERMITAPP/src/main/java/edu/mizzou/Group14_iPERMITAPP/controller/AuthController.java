0665047ba6821b53aedc2bc494f3392c1d5d63a0
Zach Goodson <quantamstudios@gmail.com>
2026-04-09 21:34:52 -0500

    Better Login Page $ Prep For Functionality

--- Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/controller/AuthController.java
+++ Group14_iPERMITAPP/src/main/java/edu/mizzou/Group14_iPERMITAPP/controller/AuthController.java
@@ -1,13 +1,35 @@
 package edu.mizzou.Group14_iPERMITAPP.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AuthController {
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public String loginPage() {
+        return "login"; // loads login.html
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(@RequestParam String email,
+                              @RequestParam String password) {
+
+        // TODO: replace with real authentication
+        if (email.equals("test@test.com") && password.equals("1234")) {
+            return "redirect:/home";
+        }
+
+        return "redirect:/login?error=true";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@RequestParam String email,
+                                 @RequestParam String password) {
+
+        // TODO: save user to database
+        System.out.println("Registering: " + email);
+
+        return "redirect:/login?registered=true";
     }
 }
